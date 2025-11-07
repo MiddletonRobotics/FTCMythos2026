370d7c730295ad8c9a2289a427faceeeeeb9ec6e
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-11-06 22:50:48 -0500

    reorganize and properly initialize

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContoller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContoller.java
@@ -1,36 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.bylazar.configurables.annotations.IgnoreConfigurable;
 import com.bylazar.gamepad.Gamepad;
-import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
 import com.seattlesolvers.solverslib.command.button.GamepadButton;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
+import org.firstinspires.ftc.library.utilities.OpModeCommand;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.subsystems.Superstructure;
+import org.firstinspires.ftc.teamcode.subsystems.Turret;
 
-public class RobotContoller extends CommandOpMode {
+public class RobotContoller extends OpModeCommand {
+    private Drivetrain drivetrain;
+    private Intake intake;
+    private Turret turret;
+    private Shooter shooter;
+
+    private Superstructure superstructure;
+
     private GamepadEx driverController;
     private GamepadEx operatorController;
 
-    private GamepadButton intakeButton;
-    private GamepadButton stopIntake;
-    private GamepadButton startFlywheels;
-
-    private Intake intake = Intake.getInstance(hardwareMap);
-    private Shooter shooter = Shooter.getInstance(hardwareMap);
+    @IgnoreConfigurable
+    static TelemetryManager telemetryManager;
 
     @Override
     public void initialize() {
+        telemetryManager = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        drivetrain = Drivetrain.getInstance(hardwareMap, telemetryManager);
+        intake = Intake.getInstance(hardwareMap);
+        turret = Turret.getInstance(hardwareMap);
+        shooter = Shooter.getInstance(hardwareMap);
+
+        superstructure = new Superstructure(drivetrain, intake, turret, shooter, telemetryManager);
+
         driverController = new GamepadEx(gamepad1);
         operatorController = new GamepadEx(gamepad2);
 
-        intakeButton = new GamepadButton(driverController, GamepadKeys.Button.LEFT_BUMPER);
-        stopIntake = new GamepadButton(driverController, GamepadKeys.Button.RIGHT_BUMPER);
-        startFlywheels = GamepadButton(driverController, GamepadKeys.Button)
+        //driverController.getGamepadButton(GamepadKeys.Button.A).whenPressed()
 
-
-        intakeButton.whenPressed(() -> intake.setIntakeTargetRPM(465));
-        stopIntake.whenPressed(() -> intake.setIntakeTargetRPM(0));
     }
 }
