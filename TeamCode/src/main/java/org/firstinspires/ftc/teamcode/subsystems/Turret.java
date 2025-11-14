92c1c4a1015f793d19d44a52fa0b4e9e9f72ba69
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-11-14 08:07:20 -0500

    added all the boiler plate needed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.seattlesolvers.solverslib.controller.PIDFController;
 import com.seattlesolvers.solverslib.controller.wpilibcontroller.SimpleMotorFeedforward;
 import com.seattlesolvers.solverslib.hardware.motors.Motor;
@@ -12,24 +16,26 @@ import org.firstinspires.ftc.library.command.Commands;
 import org.firstinspires.ftc.teamcode.constants.IntakeConstants;
 import org.firstinspires.ftc.teamcode.constants.TurretConstants;
 
-public class Turret {
-    private MotorEx turretMotor;
-    private Motor.Encoder turretEncoder;
+public class Turret extends SubsystemBase {
+    private DcMotorEx turretMotor;
     private DigitalChannel leftHomingSwitch;
-    private DigitalChannel rightHomingSwitch;
 
     public enum SystemState {
         IDLE,
+        HOME,
         FINDING_POSITION,
         RELOCALIZING,
-        TARGET_POSITION
+        TARGET_POSITION,
+        MANUAL
     }
 
     public enum WantedState {
         IDLE,
+        HOME,
         FINDING_POSITION,
         RELOCALIZING,
-        TARGET_POSITION
+        TARGET_POSITION,
+        MANUAL
     }
 
     private PIDFController positionController;
@@ -47,14 +53,54 @@ public class Turret {
         return instance;
     }
 
+    private double tx;
+    private boolean hasTarget;
+
     private Turret(HardwareMap hMap) {
-        turretMotor = new MotorEx(hMap, TurretConstants.turretMotorID, Motor.GoBILDA.BARE);
-        turretMotor.setInverted(true);
+        turretMotor = hMap.get(DcMotorEx.class, TurretConstants.turretMotorID);
+        turretMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         positionController = new PIDFController(0.01,0,0,0);
         frictionController = new SimpleMotorFeedforward(0,0,0);
+    }
 
-        turretEncoder = turretMotor.encoder;
+    @Override
+    public void periodic() {
+        systemState = handleTransition();
+        applyStates();
+    }
+
+    private SystemState handleTransition() {
+        switch(wantedState) {
+            case IDLE:
+                turretMotor.setPower(0);
+                systemState =  SystemState.IDLE;
+            case HOME:
+                systemState = SystemState.HOME;
+            case FINDING_POSITION:
+                systemState = SystemState.FINDING_POSITION;
+            case RELOCALIZING:
+                systemState = SystemState.RELOCALIZING;
+            case TARGET_POSITION:
+                systemState = SystemState.TARGET_POSITION;
+            case MANUAL:
+                systemState = SystemState.MANUAL;
+        }
+
+        return systemState;
+    }
+
+    public void applyStates() {
+        switch(systemState) {
+            case IDLE:
+            case HOME:
+            case FINDING_POSITION:
+            case RELOCALIZING:
+            case TARGET_POSITION:
+            case MANUAL:
+        }
     }
 
     public void setTargetPosition(double targetPosition) {
