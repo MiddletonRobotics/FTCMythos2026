6e07dc0ced96107918e0ed67bd278720a625bc94
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-11-13 22:14:45 -0500

    new subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Transfer.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.hardware.ServoEx;
+
+public class Transfer extends SubsystemBase {
+    private ServoEx kickerServo;
+    private ServoEx blockerServo;
+}
