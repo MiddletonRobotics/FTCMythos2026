70a2892f7a9a9318c51f73bc49cae6307bdbcb62
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-11-27 22:44:36 -0500

    built out typical autoroutine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoRoutine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoRoutine.java
@@ -1,17 +1,23 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.library.command.Command;
+import org.firstinspires.ftc.library.math.Pair;
 import org.firstinspires.ftc.library.math.geometry.Pose2d;
+
+import java.util.function.Function;
 
 public class AutoRoutine {
     private final Auto auto;
     private final String name;
+    private final Function<AutoFactory, Pair<Pose, Command>> commandFactory;
 
-    public AutoRoutine(final Auto auto, final String name, Function<AutoFactory, Pair<Pose2d, Command>> commandFactory) {
+    public AutoRoutine(final Auto auto, final String name, final Function<AutoFactory, Pair<Pose, Command>> commandFactory) {
         this.auto = auto;
         this.name = name;
+        this.commandFactory = commandFactory;
     }
 
     public Auto getAuto() {
@@ -34,7 +40,7 @@ public class AutoRoutine {
         return commandFactory.apply(autoFactory).getSecond();
     }
 
-    public Pose2d getStartingPose(final AutoFactory autoFactory) {
+    public Pose getStartingPose(final AutoFactory autoFactory) {
         return commandFactory.apply(autoFactory).getFirst();
     }
 }
