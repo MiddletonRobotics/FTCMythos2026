f1f2cf9afa765f4a01ea7e991ba9fba8171bf36d
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-12-01 13:42:56 -0500

    added location with embedded starting pose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Location.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Location.java
@@ -1,6 +1,22 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.pedropathing.geometry.Pose;
+
+import org.firstinspires.ftc.teamcode.constants.DrivetrainConstants;
+import org.firstinspires.ftc.teamcode.constants.GlobalConstants;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+
 public enum Location {
-    CLOSE,
-    FAR
+    CLOSE(DrivetrainConstants.kCloseStartingPoseBlue),
+    FAR(DrivetrainConstants.kFarStartingPoseBlue);
+
+    private final Pose pose;
+
+    private Location(final Pose pose) {
+        this.pose = pose;
+    }
+
+    public Pose getPose() {
+        return pose;
+    }
 }
