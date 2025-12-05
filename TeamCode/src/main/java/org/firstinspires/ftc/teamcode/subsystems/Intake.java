6f0f3cc685535954095647fac93b3e35fe42919e
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2025-12-05 15:39:31 -0500

    fixed intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -6,15 +6,18 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.library.command.SubsystemBase;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
     private DcMotorEx intakeMotor;
 
+    private Telemetry telemetry;
+
     public static Intake instance;
-    public static synchronized Intake getInstance(HardwareMap hMap) {
+    public static Intake getInstance(HardwareMap hMap, Telemetry telemetry) {
         if(instance == null) {
-            instance = new Intake(hMap);
+            instance = new Intake(hMap, telemetry);
         }
 
         return instance;
@@ -22,18 +25,26 @@ public class Intake extends SubsystemBase {
 
     private double targetPower;
 
-    private Intake(HardwareMap hMap) {
+    private Intake(HardwareMap hMap, Telemetry telemetry) {
         intakeMotor = hMap.get(DcMotorEx.class, IntakeConstants.intakeMotorID);
-        intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        this.telemetry = telemetry;
     }
 
     @Override
     public void periodic() {
     }
 
-    public void setIntakeTargetRPM(double power) {
-        intakeMotor.setPower(power);
+    public void setVelocitySetpoint(double targetRPM) {
+        telemetry.addData("ShooterVelocitySetpoint", targetRPM);
+        //intakeMotor.setPower(shooterPIDFController.calculate(getVelocity(), targetRPM) + shooterFeedforward.calculate(getVelocity()));
+    }
+
+    public void setOpenLoopSetpoint(double speed) {
+        telemetry.addData("ShooterOpenLoopSetpoint", speed);
+        intakeMotor.setPower(speed);
     }
 
     public double getVelocity() {
